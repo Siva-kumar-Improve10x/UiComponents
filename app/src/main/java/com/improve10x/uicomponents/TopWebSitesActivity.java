32d85479fd3a4fd7b8392677d595546b907fcc64
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-02-03 16:13:39 +0530

    Completed doing Time picker , Date picker , Seek bar , Rating Bar and top web sites.

--- app/src/main/java/com/improve10x/uicomponents/TopWebSitesActivity.java
+++ app/src/main/java/com/improve10x/uicomponents/TopWebSitesActivity.java
@@ -7,7 +7,12 @@ import android.widget.Button;
 
 public class TopWebSitesActivity extends AppCompatActivity {
     Button googleBtn;
+    Button faceBookBtn;
+    Button youTubeBtn;
+    Button twitterBtn;
+    Button instagramBtn;
     WebView webViewWv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +20,55 @@ public class TopWebSitesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_web_sites);
         handleInit();
         handleGoogleBtn();
+        handleFacebookBtn();
+        handleYoutubeBtn();
+        handleTwitterBtn();
+        handleInstagramBtn();
+
 
 
     }
 
+    private void handleInstagramBtn() {
+        instagramBtn.setOnClickListener(v -> {
+            webViewWv.loadUrl("https://www.instagram.com/");
+        });
+
+    }
+
+    private void handleTwitterBtn() {
+        twitterBtn.setOnClickListener(v -> {
+            webViewWv.loadUrl("https://twitter.com/login?lang=en");
+        });
+
+    }
+
+    private void handleYoutubeBtn() {
+        youTubeBtn.setOnClickListener(v -> {
+            webViewWv.loadUrl("https://www.youtube.com/");
+        });
+    }
+
+    private void handleFacebookBtn() {
+        faceBookBtn.setOnClickListener(v -> {
+            webViewWv.loadUrl("https://www.facebook.com/");
+        });
+    }
+
     private void handleGoogleBtn() {
         googleBtn.setOnClickListener(v ->{
+            webViewWv.loadUrl("https://www.google.com/");
 
         } );
     }
 
     private void handleInit() {
-        googleBtn.findViewById(R.id.google_btn);
-        webViewWv.findViewById(R.id.webview_wv);
+        googleBtn = findViewById(R.id.google_btn);
+        webViewWv = findViewById(R.id.webview_wv);
+        faceBookBtn = findViewById(R.id.facebook_btn);
+        youTubeBtn = findViewById(R.id.youtube_btn);
+        twitterBtn = findViewById(R.id.twitter_btn);
+        instagramBtn = findViewById(R.id.instagram_btn);
 
 
     }
